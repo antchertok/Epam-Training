5450fd0d8ac3c8097755b39db01a1234f0644d1b
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-04-21 19:37:52 +0300

    Task 1

--- task1/model/entity/AccumulativeInsurance.java
+++ task1/model/entity/AccumulativeInsurance.java
@@ -1,6 +1,6 @@
 package by.epam.task1.model.entity;
 
-public class AccumulativeInsurance extends Insurance {
+public class AccumulativeInsurance extends Obligation {
 
     private static final int RISK_LEVEL = 0;
 
@@ -10,7 +10,7 @@ public class AccumulativeInsurance extends Insurance {
 
     @Override
     public String toString() {
-        return "Accumulative insurance. Sphere: " + insuranceSphere + ", risk level:"
+        return "Accumulative insurance. Sphere: " + obligationSphere + ", risk level:"
                 + RISK_LEVEL + ", cost:" + cost;
     }
 }
