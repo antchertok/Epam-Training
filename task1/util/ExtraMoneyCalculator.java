252f4fe93a42333f3200d2aea8c38a3cce9041fd
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-05-06 15:20:36 +0300

    Update ExtraMoneyCalculator.java

--- task1/util/ExtraMoneyCalculator.java
+++ task1/util/ExtraMoneyCalculator.java
@@ -4,6 +4,13 @@ import by.epam.task1.model.entity.AccumulativeInsurance;
 
 public class ExtraMoneyCalculator {
 
+    /**
+     * Calculates how much money accumulative derivative gained by this day
+     *
+     * @param aInsurance accumulative insurance under consideration
+     * @param firstCost initial cost
+     * @return initial cost plus extra money gained currently
+     */
     public static int calcExtraMoney(AccumulativeInsurance aInsurance, double firstCost) {
         int yearsPassed = aInsurance.getYearsPassed();
         double multiplier = 1 + yearsPassed / 100.;
