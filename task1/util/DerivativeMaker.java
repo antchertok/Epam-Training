df355e4854ccef8bb3f9e60f86bec7add3d8201d
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-05-06 15:20:17 +0300

    Update DerivativeMaker.java

--- task1/util/DerivativeMaker.java
+++ task1/util/DerivativeMaker.java
@@ -1,6 +1,5 @@
 package by.epam.task1.util;
 
-
 import by.epam.task1.model.Derivative;
 import by.epam.task1.model.InsuranceSphere;
 import by.epam.task1.model.entity.Insurance;
@@ -9,9 +8,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
+
 public class DerivativeMaker {
     private static final int MIN_COST = 100;
 
+    /**
+     * Makes derivative for testing business-logic
+     *
+     * @param amountOfInsurances how many insurances should has derivative
+     * @param possibleCost how expensive each insurance can be
+     * @param possibleRisk how risky each insurance can be
+     * @return created derivative
+     */
     public Derivative makeDerivative(int amountOfInsurances, int possibleCost, int possibleRisk) {
         List<Insurance> insurances = new ArrayList<>();
         Random random = new Random();
