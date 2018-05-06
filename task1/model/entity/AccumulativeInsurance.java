fc49606ce2cb0e30725c6adfb582e52b21516846
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-05-06 15:07:54 +0300

    Update AccumulativeInsurance.java

--- task1/model/entity/AccumulativeInsurance.java
+++ task1/model/entity/AccumulativeInsurance.java
@@ -2,6 +2,13 @@ package by.epam.task1.model.entity;
 
 import by.epam.task1.util.ExtraMoneyCalculator;
 
+import java.util.Objects;
+
+/**
+ * An instance of this class represents a single  accumulative insurance
+ * Extends {@link Insurance Insurance}
+ */
+
 public class AccumulativeInsurance extends Insurance {
     private static final int RISK_LEVEL = 0;
     private int riskLevel;
@@ -18,8 +25,9 @@ public class AccumulativeInsurance extends Insurance {
 
     @Override
     public String toString() {
-        return getClass().getName()+ "Accumulative insurance. Sphere: " + insuranceSphere + ", risk level:"
-                + RISK_LEVEL + ", cost:" + getCost();
+        return getClass().getName() + "Accumulative insurance. Sphere: " + insuranceSphere 
+                + ", persents per year:" + percentsPerYear + ", years passed:" 
+                + yearsPassed + ", cost:" + getCost();
     }
 
     public int getYearsPassed() {
@@ -37,6 +45,24 @@ public class AccumulativeInsurance extends Insurance {
     @Override
     public int getCost() {
         return ExtraMoneyCalculator.calcExtraMoney(this, cost);
+    }
+
+    @Override
+    public int hashCode() {
+        return 17 * riskLevel + 13 * cost + 7 * percentsPerYear
+                + 5 * yearsPassed + insuranceSphere.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!super.equals(obj)) return false;
+        if (this == obj) return true;
+        if (null == obj) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        AccumulativeInsurance aInsurance = (AccumulativeInsurance) obj;
+        return percentsPerYear == aInsurance.percentsPerYear
+                && yearsPassed == aInsurance.yearsPassed;
     }
 
 }
