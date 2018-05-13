167d89ad3911aa6e4037893180f88a42a3915025
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-05-13 23:35:13 +0300

    Update InsuranceSphere.java

--- task1/model/InsuranceSphere.java
+++ task1/model/InsuranceSphere.java
@@ -1,5 +1,8 @@
 package by.epam.task1.model;
 
+/**
+ * Enumeration of possible insurance spheres for generating new insurances.
+ */
 public enum InsuranceSphere {
     HEALTH, BUSINESS, PROPERTY
 }
