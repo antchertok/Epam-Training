843fa31b38c0e5dc93ae4623ace618611110e905
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-06-04 20:19:55 +0300

    Update SentenceParser.java

--- task2/util/parser/SentenceParser.java
+++ task2/util/parser/SentenceParser.java
@@ -25,7 +25,7 @@ public class SentenceParser extends Parser {
         ResourceBundle bundle = ResourceBundle.getBundle("regex", Locale.ENGLISH);
         Matcher matcher = Pattern.compile(bundle.getString("sentence")).matcher(text);
         Parser nextParser = getNextParser();
-        ArrayList<Component> branches = new ArrayList<>();
+        Composite branches = new Composite();
 
         if (nextParser != null) {
             while (matcher.find()) {
@@ -36,7 +36,7 @@ public class SentenceParser extends Parser {
                 branches.add(new Leaf(matcher.group()));
             }
         }
-        return new Composite(branches);
+        return branches;
     }
 }
 
