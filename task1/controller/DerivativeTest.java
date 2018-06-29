4d1f7e87369df3ef3996ab57fd4300606eb066c6
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-06-30 00:03:03 +0300

    Update DerivativeTest.java

--- task1/controller/DerivativeTest.java
+++ task1/controller/DerivativeTest.java
@@ -3,23 +3,24 @@ package by.epam.task1.controller;
 import by.epam.task1.model.entity.AccumulativeInsurance;
 import by.epam.task1.model.entity.Insurance;
 import by.epam.task1.model.Derivative;
-import by.epam.task1.util.DerivativeMaker;
 import by.epam.task1.util.InsuranceFinder;
+import by.epam.task1.util.parser.DerivativeDomParser;
+import by.epam.task1.util.parser.Director;
 import org.apache.log4j.Logger;
 
 import java.util.Comparator;
 
 
 public class DerivativeTest {
-    private static final Logger logger = Logger.getLogger(DerivativeTest.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DerivativeTest.class.getName());
+    private static final String PATH = "C:\\Users\\-\\IdeaProjects\\StartProjects\\src\\by\\epam\\task1\\data\\deriv.xml";
 
     public static void main(String[] args) {
-        DerivativeMaker derivativeMaker = new DerivativeMaker();
-        Derivative derivative = derivativeMaker.makeDerivative(10, 2000, 100);
+        Derivative derivative = new Director(new DerivativeDomParser()).parse(PATH);
 
-        logger.info("TOTAL: " + derivative.calculateTotal());
+        LOGGER.info("TOTAL: " + derivative.calculateTotal());
 
-        logger.info(derivative.toString());
+        LOGGER.info(derivative.toString());
         derivative.addInsurance(new AccumulativeInsurance(1250, "business", 3, 5));
 
 
@@ -30,10 +31,10 @@ public class DerivativeTest {
             }
         });
 
-        logger.info(derivative.toString());
+        LOGGER.info(derivative.toString());
 
         InsuranceFinder insuranceFinder = new InsuranceFinder();
-        logger.info(insuranceFinder.findInsurancesWithCost(derivative, 1200, 1700));
+        LOGGER.info(insuranceFinder.findInsurancesWithCost(derivative, 1200, 1700));
     }
 
 }
