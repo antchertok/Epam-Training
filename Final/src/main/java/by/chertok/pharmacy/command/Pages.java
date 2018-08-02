9effa55199b52104fb9086b73dab04558edc73d6
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-08-02 15:47:37 +0300

    Add files via upload

---
+++ Final/src/main/java/by/chertok/pharmacy/command/Pages.java
@@ -0,0 +1,10 @@
+package main.java.by.chertok.pharmacy.command;
+
+public interface Pages {
+    String ERROR = "WEB-INF/page/error.jsp";
+    String START_PAGE = "WEB-INF/page/index.jsp";
+    String ALTERNATING_DRUGS = "WEB-INF/page/drug-alternate.jsp";
+    String PROFILE = "WEB-INF/page/profile.jsp";
+    String LOGIN = "WEB-INF/page/login.jsp";
+    String PRESCRIPTIONS = "WEB-INF/page/prescriptions.jsp";
+}
